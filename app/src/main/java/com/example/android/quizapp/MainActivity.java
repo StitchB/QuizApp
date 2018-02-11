4b6af6fe979c4456d4c4c51aa8e42924904d3d51
StitchB <stitchb.dev@gmail.com>
2018-02-11 18:17:31 +0100

    Use Glide library for image loading

---
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.quizapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Hide action bar
+        hideActionBar();
+    }
+
+    /**
+     * Hide action bar
+     */
+    private void hideActionBar() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    protected void loadGlideImage(Context context, int image, ImageView container) {
+        GlideApp.with(context)
+                .load(image)
+                .into(container);
+    }
+}
